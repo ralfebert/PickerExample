ea27467a9a9b09cbf195abba97df9ec8479ed24e
Ralf Ebert <info@ralfebert.de>
2021-07-26 14:59:24 +0200

    examples for all kinds of pickers

--- PickerExample/EnumPickerExampleView.swift
+++ PickerExample/EnumPickerExampleView.swift
@@ -1,23 +1,91 @@
 import SwiftUI
 
-enum Direction: String, CaseIterable {
-    case north
-    case south
-    case east
-    case west
+enum CardSuit: String, CaseIterable {
+    case diamonds, clubs, hearts, spades
+
+    var symbol: String {
+        switch self {
+        case .diamonds:
+            return "♦️"
+        case .clubs:
+            return "♣️"
+        case .hearts:
+            return "♥️"
+        case .spades:
+            return "♠️"
+        }
+    }
+
+    var title: String {
+        "\(symbol) \(rawValue.capitalized)"
+    }
 }
 
 struct EnumPickerExampleView: View {
-    @State var direction = Direction.north
+    @State var suit: CardSuit = .diamonds
 
     var body: some View {
-        Picker("Direction", selection: $direction) {
-            ForEach(Direction.allCases, id: \.self) { direction in
-                Text(direction.rawValue)
-                    .tag(direction)
+        VStack(spacing: 30) {
+            VStack {
+                Text("Picker with SegmentedPickerStyle")
+
+                Picker("Suit", selection: $suit) {
+                    ForEach(CardSuit.allCases, id: \.self) { suit in
+                        Text(suit.title)
+                            .tag(suit)
+                    }
+                }
+                .pickerStyle(SegmentedPickerStyle())
+            }
+
+            Divider()
+
+            VStack {
+                Text("Picker with MenuPickerStyle")
+
+                Picker(suit.title, selection: $suit) {
+                    ForEach(CardSuit.allCases, id: \.self) { suit in
+                        Text(suit.title)
+                            .tag(suit)
+                    }
+                }
+                .pickerStyle(MenuPickerStyle())
+            }
+            Divider()
+
+            VStack {
+                Text("Menu")
+
+                Menu(
+                    content: {
+                        ForEach(CardSuit.allCases, id: \.self) { suit in
+                            Button(suit.title) {
+                                self.suit = suit
+                            }
+                        }
+                    },
+                    label: {
+                        Text(self.suit.title)
+                    }
+                )
+            }
+
+            Divider()
+
+            Text("Context Menu (long press to select)")
+
+            VStack {
+                Text(self.suit.title)
+                    .contextMenu {
+                        ForEach(CardSuit.allCases, id: \.self) { suit in
+                            Button(suit.title) {
+                                self.suit = suit
+                            }
+                        }
+                    }
             }
         }
-        .pickerStyle(SegmentedPickerStyle())
+        .padding()
     }
 }
 
